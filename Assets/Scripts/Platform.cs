35a541c7fa1d3993fc12bbcb38abc9a556e5ae1e
RubySteak <evpad92@student.oaisd.org>
2025-05-08 22:06:48 -0400

    Changed a few things and added OnCollisionStay

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    private Collider2D platformCollider;
+
+    [SerializeField]
+    private float disableDuration = 0.5f; // Duration for which the collider is disabled
+
+    private bool playerOnPlatform = false; // Tracks if the player is on the platform
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformCollider = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerOnPlatform && Input.GetKeyDown(KeyCode.S))
+        {
+            StartCoroutine(DisableCollider());
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            playerOnPlatform = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            playerOnPlatform = false;
+        }
+    }
+
+    private IEnumerator DisableCollider()
+    {
+        platformCollider.enabled = false;
+        yield return new WaitForSeconds(disableDuration);
+        platformCollider.enabled = true;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -88,7 +88,7 @@ public class Player : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionStay2D(Collision2D other)
     {
         
         if (other.gameObject.CompareTag("Ground"))

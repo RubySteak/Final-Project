5e3fda7166f22e499f22aafbc3713f314f474137
RubySteak <evpad92@student.oaisd.org>
2025-05-19 21:43:17 -0400

    Started on level 1

---
+++ Assets/Scripts/MoveLeftRight.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeftRight : MonoBehaviour
+{
+    public float speed = 2f;
+    public float distance = 3f;
+    
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        float offset = Mathf.Sin(Time.time * speed) * distance;
+        transform.position = startPos + Vector3.right * offset;
+    }
+}
